58a76c561658b9cf62f9163be4c8e6e407abe704
ericssendelima <ericssendelima@gmail.com>
2025-02-26 11:19:20 -0300

    new Date(date_string)

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/api", function (req, res) {
 
 app.get("/api/:date", function (req, res) {
   let date_string = req.params.date;
-  let nUnixDate = new Date(req.params.date);
+  let nUnixDate = new Date(date_string);
 
   if (req.params.date.includes("-")) {
     //gerando uma chave unix a partir de uma data
