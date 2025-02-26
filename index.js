bb20f9704c25131c4838c5b60c4d4f24d93076e4
ericssendelima <ericssendelima@gmail.com>
2025-02-26 11:11:15 -0300

    atualização node

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 // where your node app starts
 
 // init project
-var express = require("express");
-var app = express();
+import express from "express";
+import cors from 'cors';
+import path from 'path';
+
+const __dirname = path.resolve();
+const app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
-var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -49,6 +52,6 @@ app.get("/api/:date", function (req, res) {
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3033, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
